20bab69bfc34c835cf87fdb9d68121027e8d0f5e
Yuvraj Bhonsle <yuvrajyhb@gmail.com>
2024-05-24 01:18:33 +0530

    fix: grid last section height and some height & width changes in other grid sections

--- src/components/GridView/fourthGrid.tsx
+++ src/components/GridView/fourthGrid.tsx
@@ -3,12 +3,12 @@ import { FOURTH_GRID_HEADLINE } from "../constatnts";
 
 function FourthGrid() {
   return (
-    <div className="pt-4 flex flex-col justify-between gap-4 h-full w-full text-2xl items-center">
-      <h1 className="font-primary-medium md:self-start self-center text-center md:text-start w-[55%] px-6 pt-2">
+    <div className="flex flex-col justify-between gap-4 md:gap-0 h-full w-full text-2xl items-center">
+      <h1 className="font-primary-medium md:self-start self-center text-center md:text-start w-[55%] px-6 pt-6">
         {FOURTH_GRID_HEADLINE}
       </h1>
-      <section className="w-[70%]">
-        <img src={"/fourth_grid_card.png"} className="h-full" alt="grid-card" />
+      <section className="md:w-[75%] w-[70%] h-[65%]">
+        <img src={"/fourth_grid_card.png"} className="h-full object-fill object-center" alt="grid-card" />
       </section>
     </div>
   );
