96bf69eb07f8724863dd45a0b1b5b300d11807e1
Vivek Suthar <sutharvivek18@gmail.com>
2024-05-28 00:14:07 +0530

    move sections data to constants

--- src/components/Sections/index.tsx
+++ src/components/Sections/index.tsx
@@ -1,40 +1,5 @@
+import { sectionData } from "@/constants/sections";
 import SectionContainer from "@/utils/sectionContainer";
-
-const sectionData = [
-  {
-    title: "Seamless Cashback Across Multiple Chains!",
-    description:
-      "Earn orbs effortlessly and watch your cashback soar! The more orbs you hold, the higher your cashback percentage. It's that simple, that exciting!",
-    universalCashbackText: "Universal Cashback",
-    crossChainBenefitsText: "Cross-Chain Benefits",
-    mainImageSrc: "/sections_img_1.webp",
-
-    pointsOneImageSrc: "/sections_universal.webp",
-    pointsTwoImageSrc: "/sections_universal_link.webp",
-  },
-  {
-    title: "Empowers Privacy, Ensures Authority",
-    description:
-      "Discover the power of privacy and authority with our platform. Take control of your data and decisions, navigating the digital world with confidence. With us, your privacy is protected, your authority respected. Embrace empowerment in the digital age.",
-    universalCashbackText: "Privacy Empowerment",
-    crossChainBenefitsText: "Authority Assurance",
-    mainImageSrc: "/sections_img_2.webp",
-
-    pointsOneImageSrc: "/sections_empowers_lock.webp",
-    pointsTwoImageSrc: "/sections_empowers_privacy.webp",
-  },
-  {
-    title: "Swap Crypto With Zero Fees",
-    description:
-      "Swap cryptocurrencies effortlessly, without any fees. Enjoy seamless transactions and maximize your assets without worrying about extra charges. Experience the freedom of fee-free crypto swapping today!",
-    universalCashbackText: "Fee-Free Swapping",
-    crossChainBenefitsText: "Maximize Your Assets",
-    mainImageSrc: "/sections_img_3.webp",
-
-    pointsOneImageSrc: "/sections_crypto_swap.webp",
-    pointsTwoImageSrc: "/sections_crypto_wallet.webp",
-  },
-];
 
 function Sections() {
   return <SectionContainer data={sectionData} />;
